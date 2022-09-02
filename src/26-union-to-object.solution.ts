c926be5a2e86be191feb2289243655dbb6f666bd
Matt Pocock <mattpocockvoice@gmail.com>
2022-09-02 10:41:22 +0100

    WIP

--- src/26-union-to-object.solution.ts
+++ src/26-union-to-object.solution.ts
@@ -2,7 +2,9 @@ import { Equal, Expect } from "./helpers/type-utils";
 
 type Route = "/" | "/about" | "/admin" | "/admin/users";
 
-type RoutesObject = unknown;
+type RoutesObject = {
+  [R in Route]: R;
+};
 
 type tests = [
   Expect<
