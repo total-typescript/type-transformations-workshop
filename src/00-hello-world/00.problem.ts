937be4ba2cf725856593e99fc4451e35cab31e42
Matt Pocock <mattpocockvoice@gmail.com>
2022-09-16 14:44:49 +0100

    Added hello world

---
+++ src/00-hello-world/00.problem.ts
@@ -0,0 +1,8 @@
+/**
+ * Your first problem - what types are the two
+ * variables below?
+ */
+
+export const hello = "hello";
+
+export let str = "hello";
---
+++ src/00-hello-world/00.solution.ts
@@ -0,0 +1,13 @@
+export const hello = "hello";
+//           ^ 🚁
+
+export let str = "hello";
+//         ^ 🚁
+
+/**
+ * hello is typed as 'hello'
+ * str is typed as string
+ *
+ * You can figure that out by hovering over each of
+ * the variable declarations
+ */
