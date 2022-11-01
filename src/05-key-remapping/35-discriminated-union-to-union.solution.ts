69be2f49419c4b07222afbb00f7b505506bf1689
Matt Pocock <mattpocockvoice@gmail.com>
2022-11-01 13:01:06 +0000

    Changed solution to 35

--- src/05-key-remapping/35-discriminated-union-to-union.solution.ts
+++ src/05-key-remapping/35-discriminated-union-to-union.solution.ts
@@ -15,11 +15,11 @@ type Fruit =
     };
 
 type TransformedFruit = {
-  [F in Fruit["name"]]: `${F}:${Extract<Fruit, { name: F }>["color"]}`;
+  [F in Fruit as F["name"]]: `${F["name"]}:${F["color"]}`;
 }[Fruit["name"]];
 
 type tests = [
   Expect<
     Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
-  >,
+  >
 ];
