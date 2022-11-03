6fc209b8b06ddfa0bbe0459e46db4ec758b018a1
Matt Pocock <mattpocockvoice@gmail.com>
2022-11-03 10:50:24 +0000

    Added 32

---
+++ src/05-key-remapping/32-never-in-key-remapping.solution.ts
@@ -0,0 +1,29 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+interface Example {
+  name: string;
+  age: number;
+  id: string;
+  organisationId: string;
+  groupId: string;
+}
+
+type OnlyIdKeys<T> = {
+  [K in keyof T as K extends `${string}${"id" | "Id"}${string}`
+    ? K
+    : never]: T[K];
+};
+
+type tests = [
+  Expect<
+    Equal<
+      OnlyIdKeys<Example>,
+      {
+        id: string;
+        organisationId: string;
+        groupId: string;
+      }
+    >
+  >,
+  Expect<Equal<OnlyIdKeys<{}>, {}>>
+];
