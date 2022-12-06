018223f2f95d6aadd99cd0ec675eee4d93ff94f0
Matt Pocock <mattpocockvoice@gmail.com>
2022-12-06 14:27:29 +0000

    Fixed 20.7

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.solution.1.ts
+++
@@ -1,9 +0,0 @@
-type NonEmptyArray<T> = [T, ...T[]];
-
-export const makeEnum = (values: NonEmptyArray<string>) => {};
-
-makeEnum(["a"]);
-makeEnum(["a", "b", "c"]);
-
-// @ts-expect-error
-makeEnum([]);
