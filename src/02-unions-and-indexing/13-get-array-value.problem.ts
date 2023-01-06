a31d561057ea054c4c3b61f19d30ca2f83f6306f
Sagar Rout <sagarrouth0512@gmail.com>
2023-01-06 19:47:02 +0530

    uai-array-values

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,9 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+// to pick the exact values in the array as type, make the array as constant
+// use the index access to pick the values as types (or) use number to pick all the values as an union type
+type AppleOrBanana = typeof fruits[0 | 1];
+type Fruit = typeof fruits[number];
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
