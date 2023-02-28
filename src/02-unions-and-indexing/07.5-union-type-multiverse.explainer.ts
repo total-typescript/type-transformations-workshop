4c95328e3dd338a766de2c5771e3b2cf69127da2
Dusan Misic <dmisic@inventage.com>
2023-02-28 10:38:36 +0100

    exercise 7.5: Summarizing explanation of how union types can be interpreted

--- src/02-unions-and-indexing/07.5-union-type-multiverse.explainer.ts
+++ src/02-unions-and-indexing/07.5-union-type-multiverse.explainer.ts
@@ -1,3 +1,8 @@
 // Gabriel Vergnaud explains his idea of the union type multiverse
 
+/**
+ * Creating a union type in typescript, is like creating a multiverse of possibilities in which the code will be run. So for each union
+ * member there is a certain implementation for it and typescript can understand each branch of it.
+ */
+
 export {};
