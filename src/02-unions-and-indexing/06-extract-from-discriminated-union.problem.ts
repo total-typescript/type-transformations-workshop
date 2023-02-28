6a2f6dd999c2c00324b5ad01537352983a91375d
Dusan Misic <dmisic@inventage.com>
2023-02-28 10:30:26 +0100

    exercise 6: Extracting members of a discriminated union

--- src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
+++ src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
@@ -14,6 +14,10 @@ export type Event =
       event: KeyboardEvent;
     };
 
-type ClickEvent = unknown;
+/**
+ * Extracts any member of a union that EXTENDS (not equals) the structure given in the second parameter.
+ * If more than just one member of a union extends the structure, than a new union of the "filtered" members is returned!
+ */
+type ClickEvent = Extract<Event, { type: "click" }>;
 
 type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
