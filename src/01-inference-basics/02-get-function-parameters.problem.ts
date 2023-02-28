d80eaee947c80406f37148c19df2cb38c1ed33a8
Dusan Misic <dmisic@inventage.com>
2023-02-28 09:07:25 +0100

    exercise 2: Again taking type from the "value level (makeQuery)" to the "type level (typeof makeQuery)" and using utility types to extract the parameters as a type (in this case a tuple, because makeQuery accepts 2 parameters).

--- src/01-inference-basics/02-get-function-parameters.problem.ts
+++ src/01-inference-basics/02-get-function-parameters.problem.ts
@@ -11,7 +11,18 @@ const makeQuery = (
   },
 ) => {};
 
-type MakeQueryParameters = unknown;
+type MakeQueryParameters = Parameters<typeof makeQuery>;
+
+/**
+ * Additional info:
+ *
+ * Now that we extracted the parameters as a tuple (since makeQuery accepts 2 parameters), you could also access only the second parameter
+ * type by using indexes, such as:
+ */
+type MakeQuerySecondParameter = MakeQueryParameters[1];
+
+const parameters: MakeQueryParameters = ['testurl', { method: 'testMethod' }];
+const secondParameter: MakeQuerySecondParameter = { headers: { Location: 'some location' } };
 
 type tests = [
   Expect<
