f9ad9744c41741d042e2a65cf1d61177ed3e9938
Dusan Misic <dmisic@inventage.com>
2023-02-28 09:56:13 +0100

    exercise 1.5: small summary of the typeof keyword and the type/value levels mentioned in the video.

--- src/01-inference-basics/01.5-gabriel-typeof-keyword.explainer.ts
+++ src/01-inference-basics/01.5-gabriel-typeof-keyword.explainer.ts
@@ -1,3 +1,13 @@
 // Gabriel Vergnaud explains the typeof keyword, and the type/value level
 
+/**
+ * Type level: This is kind of the "typescript world". You can see the type world as a whole programming language in itself, that uses
+ * typescript syntax to declare/construct types.
+ *
+ * Value level: This is kind of the "javascript world" where variables, functions and so on exists.
+ *
+ * In general, the 2 levels exist separately and the only gate to cross these worlds is via the keyword "typeof", which takes a value from
+ * the value level and converts it into a type for the type level. This is a very interesting concept to think of typescript.
+ */
+
 export {};
