94292bb7eae8ddab97afc457010c461f0d577359
Dusan Misic <dmisic@inventage.com>
2023-03-01 16:24:24 +0100

    exercise 23: Using the "infer" keyword, to create a new type variable, that is accessible and modifiable inside the return body of the type function.

--- src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,6 +1,25 @@
 import { Equal, Expect } from "../helpers/type-utils";
+import { infer } from 'zod';
 
-type GetDataValue<T> = unknown;
+/**
+ * There are 2 possible solutions to this problem.
+ *
+ * This first solution look very straight forward, as you simply just check if the type has a data property in it and then access the
+ * data via index. But there is a better way.
+ */
+type GetDataValue<T> = T extends { data: any }
+  ? T['data']
+  : never;
+
+/**
+ * Here in the second solution we use typescript's keyword "infer". What this does, it infers the type that was passed in the property
+ * data and gives it a new name (in this case TData). This new named type is now available in the true case of the ternary expression
+ * (scoped) and can be used for all sorts of things. This gives you more freedom to further transform this type and work with it.
+ * Basically everytime when a type needs to be extracted you should try to use the "infer" keyword.
+ */
+type GetDataValue2<T> = T extends { data: infer TData }
+  ? TData
+  : never;
 
 type tests = [
   Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
