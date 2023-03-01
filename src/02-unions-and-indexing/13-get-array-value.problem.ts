24437303c3c42c6e43a8141d4c435ab4ba3cfbbd
Dusan Misic <dmisic@inventage.com>
2023-03-01 09:40:54 +0100

    exercise 13: accessing via index in a constant array. Also accessing ALL elements in a constant array by passing "number" as index.

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,9 +1,13 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+/**
+ * Here we can index certain types inside a constant array again via a union with the index numbers, or if we want to access ALL types
+ * and create a union type out of them, then we can simply pass "number" as index, and it will always grab all elements in an array.
+ */
+type AppleOrBanana = typeof fruits[0 | 1];
+type Fruit = typeof fruits[number];
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
