d3c0d5cfcfd5fa7c02564b9019989f44e2794f51
Dusan Misic <dmisic@inventage.com>
2023-03-01 10:20:23 +0100

    exercise 18: Use utility type "Record" and template literal to create an object with all possibilities of the template literal as a key of an object.

--- src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -2,7 +2,12 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
 
-type ObjectOfKeys = unknown;
+/**
+ * Here we use all the possibilities of the above template literal as key of an object and assign each key to be a type of string.
+ * This is done with the utility type "Record". Not that in Record, you cannot assign different type to different keys. In this case,
+ * they all will receive a type of string.
+ */
+type ObjectOfKeys = Record<TemplateLiteralKey, string>;
 
 type tests = [
   Expect<
