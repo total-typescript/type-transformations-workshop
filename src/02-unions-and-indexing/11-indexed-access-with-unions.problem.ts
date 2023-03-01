e41cf17466cc25c7fa0d78c0ccd37e73919bd88b
Dusan Misic <dmisic@inventage.com>
2023-03-01 08:43:03 +0100

    exercise 11: accessing an index via union, mixed with the Exclude utility type.

--- src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -9,12 +9,33 @@ export const programModeEnumMap = {
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
 } as const;
 
-export type IndividualProgram = unknown;
+type ProgramModeEnum = typeof programModeEnumMap;
+
+/**
+ * First solution, shows you how you can use a union indexed kind of approach. So unions can also be used inside the indexing, which in
+ * itself returns a union of all the indexed value! Pretty neat!
+ */
+export type IndividualProgram = ProgramModeEnum[
+  | 'ONE_ON_ONE'
+  | 'SELF_DIRECTED'
+  | 'PLANNED_ONE_ON_ONE'
+  | 'PLANNED_SELF_DIRECTED'
+];
+
+/**
+ * The second solution, shows you an approach with the before learned EXCLUDE utility type. We exclude all the keys of the
+ * ProgramModeEnum that we don't want, which creates a union of all keys we DO want. We can then use that union of keys, as indexes,
+ * just like we did in the first solution. This would ofc also work, if we nested the EXCLUDE operation inside the index brackets [].
+ * Pretty cool!
+ */
+type OnlyIndividualProgramKeys = Exclude<keyof ProgramModeEnum, 'GROUP' | 'ANNOUNCEMENT'>
+
+export type IndividualProgram2 = ProgramModeEnum[OnlyIndividualProgramKeys]
 
 type tests = [
   Expect<
     Equal<
-      IndividualProgram,
+      IndividualProgram2,
       "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
     >
   >,
