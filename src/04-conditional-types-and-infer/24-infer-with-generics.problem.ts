5872ae84c7abee9f3c8ca67d8cb6a751ff6c487d
Dusan Misic <dmisic@inventage.com>
2023-03-01 16:40:11 +0100

    exercise 24: Using the "infer" keyword, to simplify getting the return type

--- src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
+++ src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
@@ -14,6 +14,24 @@ type Example = MyComplexInterface<
   { x: 12; y: 14 }
 >;
 
-type GetPoint<T> = unknown;
+/**
+ * Here we can see how powerful that infer is. At first glance it seems harmless, but since we practically want to get the return type
+ * of the "getPoint" function inside "MyComplexInterface" we solved it very smoothly this way, because we are using the type level of
+ * MyComplexInterface and directly taking the type of Point, which is used as the return type for getPoint().
+ */
+type GetPoint<T> = T extends MyComplexInterface<any, any, any, infer TPoint>
+  ? TPoint
+  : never;
+
+/**
+ * If we for example wanted to do that without the "infer" keyword then it would look something like that.
+ *
+ * This of course works, but here we are dealing with the VALUE level of MyComplexInterface, because we are accessing the getPoint
+ * property inside it. This requires us to also use the ReturnType utility type. So since the VALUE level of the interface is more
+ * likely to change, due to a refactor, it is much safer to access the return type via infer instead!
+ */
+type GetPoint2<T> = T extends MyComplexInterface<any, any, any, any>
+  ? ReturnType<T['getPoint']>
+  : never;
 
 type tests = [Expect<Equal<GetPoint<Example>, { x: 12; y: 14 }>>];
