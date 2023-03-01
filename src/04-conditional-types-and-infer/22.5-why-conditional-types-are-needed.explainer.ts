88856a04501f9821e1087ec60837a17fb766d259
Dusan Misic <dmisic@inventage.com>
2023-03-01 15:35:19 +0100

    exercise 22.5: Summary of the video

--- src/04-conditional-types-and-infer/22.5-why-conditional-types-are-needed.explainer.ts
+++ src/04-conditional-types-and-infer/22.5-why-conditional-types-are-needed.explainer.ts
@@ -1,3 +1,8 @@
 // Daniel Rosenwasser explains why conditional types were needed in TS
 
+/**
+ * Daniel Rosenwasser is a developer of typescript and explained how thanks to Knockout.js, the conditional types actually got created.
+ * Because they needed to know whether a type extend an array or an object in order to do something, otherwise just ignore it.
+ */
+
 export {};
