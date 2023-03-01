281747e23f7db2004a5cbba1ac943bb2c06a98d6
Dusan Misic <dmisic@inventage.com>
2023-03-01 15:23:34 +0100

    exercise 21: Using "extends" in the return body of the type function and combine it with a ternary operator.

--- src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,6 +1,10 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello = unknown;
+/**
+ * Here we first time use the "extends" keyword in the return body, of the type function. Here it also sort of acts like matcher and can
+ * be used to do a ternary operator.
+ */
+type YouSayGoodbyeAndISayHello<T extends 'hello' | 'goodbye'> = T extends 'hello' ? 'goodbye' : 'hello';
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
