b89938a2a31d92ed3752b3e2f02d3a833ef60373
Dusan Misic <dmisic@inventage.com>
2023-03-01 16:59:48 +0100

    exercise 25.5: Video summary

--- src/04-conditional-types-and-infer/25.5-regex-types.explainer.ts
+++ src/04-conditional-types-and-infer/25.5-regex-types.explainer.ts
@@ -1,3 +1,7 @@
 // Daniel Rosenwasser explains why regex types WEREN'T added to TypeScript
 
+/**
+ * They basically didn't want to use regex, because nobody gets them and also uses them correctly.
+ */
+
 export {};
