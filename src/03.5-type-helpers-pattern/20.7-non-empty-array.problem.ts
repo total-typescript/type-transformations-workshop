c407d19fb5eba6c7d9ee9323955a2e2949df9580
Dusan Misic <dmisic@inventage.com>
2023-03-01 14:57:19 +0100

    exercise 20.7: Typing a non-empty array of any type.

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,4 +1,9 @@
-type NonEmptyArray = unknown;
+
+/**
+ * Here we force the returned array to have at least one element in it. This means that if the user of this type, tries to pass in an
+ * empty array, typescript will start yelling at them
+ */
+type NonEmptyArray<T> = [T, ...T[]];
 
 export const makeEnum = (values: NonEmptyArray<string>) => {};
 
