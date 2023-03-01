36b77d06daf8efd39795c76afbdeb63c65fd41af
Dusan Misic <dmisic@inventage.com>
2023-03-01 10:14:56 +0100

    exercise 17: Use a ts tool library, called ts-toolbelt to split a string by a delimiter.

--- src/03-template-literals/17-splitting-strings.problem.ts
+++ src/03-template-literals/17-splitting-strings.problem.ts
@@ -3,10 +3,11 @@
 // https://millsp.github.io/ts-toolbelt/modules/string_split.html
 
 import { Equal, Expect } from "../helpers/type-utils";
+import { S } from 'ts-toolbelt';
 
 type Path = "Users/John/Documents/notes.txt";
 
-type SplitPath = unknown;
+type SplitPath = S.Split<Path, "/">;
 
 type tests = [
   Expect<Equal<SplitPath, ["Users", "John", "Documents", "notes.txt"]>>,
