abbded5b3217abbf9b0e7c82bd4a190c6c77759f
Dusan Misic <dmisic@inventage.com>
2023-03-01 14:09:36 +0100

    exercise 20: Use generics to sort of make a "type function" that takes the generic as a kind of "argument", does something with it and returns a new type.

--- src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
+++ src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
@@ -1,6 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type ReturnWhatIPassIn = unknown;
+/**
+ * Here we use generics to kind of transform a static type into a function, which takes an argument inside the angled brackets and then
+ * returns a new type. These arguments can be used in many ways too, such as use default values, or restrict them and so on. Matt calls
+ * these kind of functions, "type helpers".
+ */
+type ReturnWhatIPassIn<TPassed> = TPassed;
 
 type tests = [
   Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
