82c7d6a522b3f66d782159459a6a9674b3cb8946
Dusan Misic <dmisic@inventage.com>
2023-03-01 09:58:06 +0100

    exercise 15: Using template literals to Exclude only certain strings with a certain pattern.

--- src/03-template-literals/15-extract-with-template-literals.problem.ts
+++ src/03-template-literals/15-extract-with-template-literals.problem.ts
@@ -2,6 +2,11 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Routes = "/users" | "/users/:id" | "/posts" | "/posts/:id";
 
-type DynamicRoutes = unknown;
+/**
+ * Here we can again use EXTRACT to get only the dynamic routes (the ones with a ":" anywhere in their path) and as the second argument
+ * in the Extract utility type, we can again use template literals to sort of create a "regex" to tell typescript to only allow string
+ * that have a ":" somewhere between strings.
+ */
+type DynamicRoutes = Extract<Routes, `${string}:${string}`>;
 
 type tests = [Expect<Equal<DynamicRoutes, "/users/:id" | "/posts/:id">>];
