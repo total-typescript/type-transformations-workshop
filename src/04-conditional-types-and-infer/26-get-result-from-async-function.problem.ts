6e7feea77733e28b5d047256dd2a0f4a7a80e362
Dusan Misic <dmisic@inventage.com>
2023-03-02 07:56:47 +0100

    exercise 26: Use keyword "infer" to extract a type from a function returning an object, with a property that is of our interest.

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -10,12 +10,36 @@ const getServerSideProps = async () => {
   };
 };
 
-type InferPropsFromServerSideFunction = unknown;
+/**
+ * Here is a powerful example, of how to extract types from a function that we might not control, so instead of copying the structure of
+ * the object we want to extract, we can instead use this sort of helper type!
+ */
+type InferPropsFromServerSideFunction<T> = T extends (...args: any) => Promise<{ props: infer PROPS }>
+  ? PROPS
+  : never;
+
+/** Example usage: */
+const serverSideData = await getServerSideProps();
+const props: InferPropsFromServerSideFunction<typeof getServerSideProps> = serverSideData.props;
+/** Why not just let TS infer it by itself? */
+const props2 = serverSideData.props;
 
 type tests = [
   Expect<
     Equal<
       InferPropsFromServerSideFunction<typeof getServerSideProps>,
+      { json: { title: string } }
+    >
+  >,
+  Expect<
+    Equal<
+      typeof props2,
+      { json: { title: string } }
+    >
+  >,
+  Expect<
+    Equal<
+      typeof props,
       { json: { title: string } }
     >
   >
