a653ec26423377d8e5a6402ad282ab63539d18d4
Dusan Misic <dmisic@inventage.com>
2023-03-02 14:29:33 +0100

    challenge 4: Recursively making all members of an object optional, with a nice gotcha for arrays.

--- src/06-challenges/40-deep-partial.problem.ts
+++ src/06-challenges/40-deep-partial.problem.ts
@@ -1,6 +1,26 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type DeepPartial<T> = unknown;
+/**
+ * So this solution is ALMOST correct, but not 100%. The reason is because, even tho we deeply made everything optional
+ * in a case where you're allowed to add additional members, you can simply do so! But we do not want someone to be able
+ * to randomly add values. So for example if one of the nested member is an array, then the user can still do for example
+ * g: [undefined, { h: '123' }], even tho we have only declared h and i in the original object (MyType). So to restrict
+ * this, we need to add a check for Arrays like in the example below.
+ */
+type AlmostDeepPartial<T> = {
+  [K in keyof T]?: DeepPartial<T[K]>
+};
+
+/**
+ * So here you can see we catch the issue with array, infer whatever is inside that array and then create a new array
+ * where the inferred content of the array is being partialised, so that it must either be empty or match exactly the
+ * structure of the original object.
+ */
+type DeepPartial<T> = T extends Array<infer A>
+  ? Array<DeepPartial<A>>
+  : {
+    [K in keyof T]?: DeepPartial<T[K]>
+  };
 
 type MyType = {
   a: string;
