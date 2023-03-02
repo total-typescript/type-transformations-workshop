c2efe202d253d5c203c512f86a9960bac05340a8
Dusan Misic <dmisic@inventage.com>
2023-03-02 09:39:55 +0100

    exercise 31: Use "as" keyword to rename the keys of a mapped type.

--- src/05-key-remapping/31-k-in-keyof-as.problem.ts
+++ src/05-key-remapping/31-k-in-keyof-as.problem.ts
@@ -6,8 +6,11 @@ interface Attributes {
   age: number;
 }
 
+/**
+ * Here we get to use the "as" keyword inside a mapped type. This lets us rename the name of the key.
+ */
 type AttributeGetters = {
-  [K in keyof Attributes]: () => Attributes[K];
+  [K in keyof Attributes as `get${Capitalize<K>}`]: () => Attributes[K];
 };
 
 type tests = [
