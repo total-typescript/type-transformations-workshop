356f33ad5cf0b55b6b54fbc83972366853e0a86e
Dusan Misic <dmisic@inventage.com>
2023-03-02 13:33:56 +0100

    challenge 2: Use mapped type to create a union once again, via indexing. But first alternating its structure to the desired shape.

--- src/06-challenges/38-mutually-exclusive-properties.problem.ts
+++ src/06-challenges/38-mutually-exclusive-properties.problem.ts
@@ -10,7 +10,20 @@ interface Attributes {
  * How do we create a type helper that represents a union
  * of all possible combinations of Attributes?
  */
-type MutuallyExclusive<T> = unknown;
+
+/**
+ * Interesting challenge. Here we again create a mapped object type, where we first try to construct each of the wanted output as value
+ * for each key. So that we can in the end, as we have learned, create a union out of it, by just accessing them via a "keyof" index.
+ */
+type MutuallyExclusive<T> = {
+  [Key in keyof T]: Record<Key, T[Key]>
+}[keyof T];
+
+/** One cool advantage of this type, is that an object that has this type MUST at least have one of the unions defined. */
+const example: MutuallyExclusive<Attributes> = {
+  id: '123',
+  email: '232'
+};
 
 type ExclusiveAttributes = MutuallyExclusive<Attributes>;
 
