2dcce1d5a8f1b0db4efab608bdbce32dea17d0e3
Dusan Misic <dmisic@inventage.com>
2023-03-02 14:04:16 +0100

    challenge 3: This time we used a ternary inside a mapped type BUT on the value side.

--- src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
+++ src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
@@ -12,7 +12,23 @@ type Route =
   | { route: "/admin" }
   | { route: "/admin/users" };
 
-type RoutesObject = unknown;
+/**
+ * So the solution I came up with here actually works pretty well, but we can take this a tiny step further.
+ * What have we said about accessing values inside a ternary expression?? (something something infer, right?)
+ */
+type RoutesObject = {
+  [R in Route as R['route']]: R extends { search: any } ? R['search'] : never
+};
+
+/**
+ * Yes, so here is the same solution but with infer, which makes it a bit prettier and more clear that we do
+ * not care what type specifically we expect there (because before we have put a random "any" there), we just
+ * want to grab whatever is there and return it. So if somebody reads our code, they don't need to worry about
+ * "what is this any type here for?".
+ */
+type RoutesObject2 = {
+  [R in Route as R['route']]: R extends { search: infer Search } ? Search : never
+};
 
 type tests = [
   Expect<
@@ -29,4 +45,16 @@ type tests = [
       }
     >
   >,
+  Equal<
+    RoutesObject2,
+    {
+      "/": {
+        page: string;
+        perPage: string;
+      };
+      "/about": never;
+      "/admin": never;
+      "/admin/users": never;
+    }
+  >
 ];
