8230f0d4affc36ccbb54f280e71ae52c5b02ebc3
Dusan Misic <dmisic@inventage.com>
2023-03-02 10:34:36 +0100

    exercise 34: Transform an object into a union.

--- src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
+++ src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
@@ -6,9 +6,16 @@ interface Values {
   lastName: string;
 }
 
+/**
+ * So this is mind-blowing. In order to get the union of the keys and values of an object as a tuple, we first need to map the
+ * given object (in this example Value) and transform its value to the desired tuple. So now we end up with an object that has the same
+ * keys, but a tuple of [key, value] as its value instead. To now create a union type that consists of all the new value (so the tuples),we
+ * simply can index access them with "keyof ObjectInterfaceName". This looks really strange, but when thinking about it, it makes a lot
+ * of sense.
+ */
 type ValuesAsUnionOfTuples = {
   [K in keyof Values]: [K, Values[K]];
-};
+}[keyof Values]; /** <- this is the magic part */
 
 type tests = [
   Expect<
