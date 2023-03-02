0eb3a526a48ea64dd51a917a175b9d32f420c6fa
Dusan Misic <dmisic@inventage.com>
2023-03-02 09:59:39 +0100

    exercise 32: Use "as" keyword conditionally extract only the wanted keys in a mapped type

--- src/05-key-remapping/32-never-in-key-remapping.problem.ts
+++ src/05-key-remapping/32-never-in-key-remapping.problem.ts
@@ -8,7 +8,18 @@ interface Example {
   groupId: string;
 }
 
-type OnlyIdKeys<T> = unknown;
+/** For better readability we created this type, which basically filters out only string that have "id" or "Id" anywhere in it. */
+type SearchForId = `${string}${'id' | 'Id'}${string}`;
+
+/**
+ * Here we now kind of combine mapped types with conditional types! We again use the "as" keyword to check whether the Key extends the
+ * type we declared above, to only grab the keys which contain an id/Id in it. If it doesn't match, we return never, which basically
+ * tells typescript to NOT include this key in our object at all. If it does match however, then we simply return Key and then use it to
+ * access the value type.
+ */
+type OnlyIdKeys<T> = {
+  [Key in keyof T as Key extends SearchForId ? Key : never ]: T[Key]
+};
 
 type tests = [
   Expect<
