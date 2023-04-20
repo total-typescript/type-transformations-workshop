c66c2cdbbfd2e0557e9898262b235b001a69205d
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-20 20:33:54 +0300

    task 6

--- src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
+++ src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
@@ -14,6 +14,6 @@ export type Event =
       event: KeyboardEvent;
     };
 
-type ClickEvent = unknown;
+type ClickEvent = Extract<Event, { type: "click" }>;
 
 type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
