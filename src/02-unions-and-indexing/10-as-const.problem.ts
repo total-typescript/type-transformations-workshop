9c0c90904219c8761accc42145faf6ee6fe5c278
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-21 12:46:37 +0300

    task 11

--- src/02-unions-and-indexing/10-as-const.problem.ts
+++ src/02-unions-and-indexing/10-as-const.problem.ts
@@ -11,16 +11,16 @@ export const programModeEnumMap = {
   SELF_DIRECTED: "selfDirected",
   PLANNED_ONE_ON_ONE: "planned1on1",
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
-};
+} as const;
 
-export type GroupProgram = typeof programModeEnumMap["GROUP"];
-export type AnnouncementProgram = typeof programModeEnumMap["ANNOUNCEMENT"];
-export type OneOnOneProgram = typeof programModeEnumMap["ONE_ON_ONE"];
-export type SelfDirectedProgram = typeof programModeEnumMap["SELF_DIRECTED"];
+export type GroupProgram = (typeof programModeEnumMap)["GROUP"];
+export type AnnouncementProgram = (typeof programModeEnumMap)["ANNOUNCEMENT"];
+export type OneOnOneProgram = (typeof programModeEnumMap)["ONE_ON_ONE"];
+export type SelfDirectedProgram = (typeof programModeEnumMap)["SELF_DIRECTED"];
 export type PlannedOneOnOneProgram =
-  typeof programModeEnumMap["PLANNED_ONE_ON_ONE"];
+  (typeof programModeEnumMap)["PLANNED_ONE_ON_ONE"];
 export type PlannedSelfDirectedProgram =
-  typeof programModeEnumMap["PLANNED_SELF_DIRECTED"];
+  (typeof programModeEnumMap)["PLANNED_SELF_DIRECTED"];
 
 type tests = [
   Expect<Equal<GroupProgram, "group">>,
@@ -28,5 +28,5 @@ type tests = [
   Expect<Equal<OneOnOneProgram, "1on1">>,
   Expect<Equal<SelfDirectedProgram, "selfDirected">>,
   Expect<Equal<PlannedOneOnOneProgram, "planned1on1">>,
-  Expect<Equal<PlannedSelfDirectedProgram, "plannedSelfDirected">>,
+  Expect<Equal<PlannedSelfDirectedProgram, "plannedSelfDirected">>
 ];
--- src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -9,7 +9,10 @@ export const programModeEnumMap = {
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
 } as const;
 
-export type IndividualProgram = unknown;
+export type IndividualProgram = (typeof programModeEnumMap)[Exclude<
+  keyof typeof programModeEnumMap,
+  "GROUP" | "ANNOUNCEMENT"
+>];
 
 type tests = [
   Expect<
@@ -17,5 +20,5 @@ type tests = [
       IndividualProgram,
       "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
     >
-  >,
+  >
 ];
