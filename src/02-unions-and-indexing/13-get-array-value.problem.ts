450ebaae6d12fefcdf9e1d55075ed0e6835cfe0c
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-21 13:49:07 +0300

    task 13

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,11 +1,12 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+type AppleOrBanana = Exclude<(typeof fruits)[number], "orange">;
+// type AppleOrBanana = (typeof fruits)[0 | 1];
+type Fruit = (typeof fruits)[number];
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
-  Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
+  Expect<Equal<Fruit, "apple" | "banana" | "orange">>
 ];
