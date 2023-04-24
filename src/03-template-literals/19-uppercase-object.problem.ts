a71c74510b9b3540b167465683394401bf50ea31
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-24 16:51:41 +0300

    task 19

--- src/03-template-literals/19-uppercase-object.problem.ts
+++ src/03-template-literals/19-uppercase-object.problem.ts
@@ -2,7 +2,7 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Event = `log_in` | "log_out" | "sign_up";
 
-type ObjectOfKeys = unknown;
+type ObjectOfKeys = Record<Uppercase<Event>, string>;
 
 type tests = [
   Expect<
@@ -14,5 +14,5 @@ type tests = [
         SIGN_UP: string;
       }
     >
-  >,
+  >
 ];
