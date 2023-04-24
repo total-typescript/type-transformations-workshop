f726aab8b7e8c8cdb13e12a5f9a17a8db06255e0
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-24 23:51:29 +0300

    task 20.3

--- src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
+++ src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
@@ -1,8 +1,8 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type CreateDataShape = {
-  data: unknown;
-  error: unknown;
+type CreateDataShape<T, D> = {
+  data: T;
+  error: D;
 };
 
 type tests = [
@@ -32,5 +32,5 @@ type tests = [
         error: SyntaxError;
       }
     >
-  >,
+  >
 ];
