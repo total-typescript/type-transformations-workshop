e5b50f368db41237402674425713ad1b128a786a
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-24 23:38:32 +0300

    task 20

--- src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
+++ src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
@@ -1,11 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type ReturnWhatIPassIn = unknown;
+type ReturnWhatIPassIn<T> = T;
 
 type tests = [
   Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
   Expect<Equal<ReturnWhatIPassIn<"1">, "1">>,
   Expect<Equal<ReturnWhatIPassIn<true>, true>>,
   Expect<Equal<ReturnWhatIPassIn<false>, false>>,
-  Expect<Equal<ReturnWhatIPassIn<null>, null>>,
+  Expect<Equal<ReturnWhatIPassIn<null>, null>>
 ];
