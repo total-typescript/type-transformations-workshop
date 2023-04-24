c2760ac89d2b605edd119c293503ac9a3db879e4
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-24 23:42:14 +0300

    task 21

--- src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
+++ src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
@@ -1,10 +1,10 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type Maybe = unknown;
+type Maybe<T> = T | null | undefined;
 
 type tests = [
   Expect<Equal<Maybe<string>, string | null | undefined>>,
   Expect<Equal<Maybe<number>, number | null | undefined>>,
   Expect<Equal<Maybe<boolean>, boolean | null | undefined>>,
-  Expect<Equal<Maybe<null>, null | undefined>>,
+  Expect<Equal<Maybe<null>, null | undefined>>
 ];
