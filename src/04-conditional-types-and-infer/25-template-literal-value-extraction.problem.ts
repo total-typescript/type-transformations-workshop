15d93afa635b495803d288d0799221c2d8f9122f
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-25 15:41:30 +0300

    task 25

--- src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
+++ src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
@@ -5,15 +5,17 @@ type Names = [
   "Jimi Hendrix",
   "Eric Clapton",
   "John Mayer",
-  "BB King",
+  "BB King"
 ];
 
-type GetSurname<T> = unknown;
+type GetSurname<T> = T extends `${infer FirstName} ${infer LastName}`
+  ? LastName
+  : never;
 
 type tests = [
   Expect<Equal<GetSurname<Names[0]>, "Pocock">>,
   Expect<Equal<GetSurname<Names[1]>, "Hendrix">>,
   Expect<Equal<GetSurname<Names[2]>, "Clapton">>,
   Expect<Equal<GetSurname<Names[3]>, "Mayer">>,
-  Expect<Equal<GetSurname<Names[4]>, "King">>,
+  Expect<Equal<GetSurname<Names[4]>, "King">>
 ];
