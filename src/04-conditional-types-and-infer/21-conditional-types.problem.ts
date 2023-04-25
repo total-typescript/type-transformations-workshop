5cfa1f91d89f7640069bd128c5dedbe46cde3e6d
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-25 10:33:10 +0300

    task 21

--- src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,8 +1,8 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello = unknown;
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>
 ];
