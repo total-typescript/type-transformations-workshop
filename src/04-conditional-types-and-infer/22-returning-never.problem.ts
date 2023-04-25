dbbfc4c24013e50132d82685c7218ccb0a67f086
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-25 10:38:07 +0300

    task 22

--- src/04-conditional-types-and-infer/22-returning-never.problem.ts
+++ src/04-conditional-types-and-infer/22-returning-never.problem.ts
@@ -1,10 +1,14 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
+type YouSayGoodbyeAndISayHello<T> = T extends "hello"
+  ? "goodbye"
+  : T extends "goodbye"
+  ? "hello"
+  : never;
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
   Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
   Expect<Equal<YouSayGoodbyeAndISayHello<"alright pal">, never>>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<1>, never>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<1>, never>>
 ];
