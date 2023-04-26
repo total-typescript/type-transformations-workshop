4ff419c492adf8edfdfe757d803e23f525ec1e15
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-26 23:17:29 +0300

    task 30

--- src/05-key-remapping/30-k-in-keyof.problem.ts
+++ src/05-key-remapping/30-k-in-keyof.problem.ts
@@ -6,7 +6,9 @@ interface Attributes {
   age: number;
 }
 
-type AttributeGetters = unknown;
+type AttributeGetters = {
+  [T in keyof Attributes]: () => Attributes[T];
+};
 
 type tests = [
   Expect<
@@ -18,5 +20,5 @@ type tests = [
         age: () => number;
       }
     >
-  >,
+  >
 ];
