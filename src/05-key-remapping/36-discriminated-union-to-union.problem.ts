38c38a7690fa35b7fb350ed746f370173af085ab
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-27 22:42:42 +0300

    task 36

--- src/05-key-remapping/36-discriminated-union-to-union.problem.ts
+++ src/05-key-remapping/36-discriminated-union-to-union.problem.ts
@@ -14,10 +14,12 @@ type Fruit =
       color: "orange";
     };
 
-type TransformedFruit = unknown;
+type TransformedFruit = {
+  [T in Fruit as T["name"]]: `${T["name"]}:${T["color"]}`;
+}[Fruit["name"]];
 
 type tests = [
   Expect<
     Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
-  >,
+  >
 ];
