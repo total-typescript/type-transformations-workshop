f3f4e3b46174813bbf1ffe20dea58689499efbfd
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-27 22:42:34 +0300

    task 37

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,10 +1,20 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
+import { S } from "ts-toolbelt";
+
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+// type SplitPath<T extends string> = S.Split<T, "/">;
+
+type sample<T extends string> = S.Split<T, "/">[number];
+
+type sample2<T> = T extends `:${infer P}` ? P : never;
+
+type result1<T> = sample2<sample<T>>;
+
+type ExtractPathParams<T> = Record<result1<T>, string>;
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
@@ -13,5 +23,5 @@ type tests = [
       ExtractPathParams<UserOrganisationPath>,
       { id: string; organisationId: string }
     >
-  >,
+  >
 ];
