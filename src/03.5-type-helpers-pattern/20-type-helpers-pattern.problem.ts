b1f8bef19994a6ea27af0cba325e7cb35fb747fc
Matt Pocock <mattpocockvoice@gmail.com>
2023-06-06 13:17:40 +0100

    Update TT CLI

--- src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
+++ src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
@@ -1,6 +1,6 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type ReturnWhatIPassIn = unknown;
+type ReturnWhatIPassIn<T> = T;
 
 type tests = [
   Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
